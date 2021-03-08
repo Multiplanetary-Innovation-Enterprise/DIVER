51caab9399423903faa6dc9f1727c7c266512340
Tyler <tylernewton08@gmail.com>
2021-03-07 23:26:21 -0500

    Removed unneeded move x and move y commands code

--- ROV/commands/MoveCommands.py
+++ ROV/commands/MoveCommands.py
@@ -20,60 +20,11 @@ class MoveXYCommand(Command):
         leftSpeed = speed * self.__leftDirection.value
         rightSpeed = speed * self.__rightDirection.value
 
-        print("Left: " + str(leftSpeed) + " Right: " + str(rightSpeed))
-
         self.__propSystem.moveLeft(leftSpeed)
         self.__propSystem.moveRight(rightSpeed)
 
     def isRepeatable(self):
         False
-
-
-# class MoveXCommand(Command):
-#     __action = None
-#     __propSystem = None
-#
-#     def __init__(self, action:Action, propSystem:PropulsionSubsystem):
-#         self.__action = action
-#         self.__propSystem = propSystem
-#
-#     def execute(self):
-#         speed = 0
-#
-#         if(self.__action == Action.MOVE_X_POS):
-#             speed = self.__propSystem.getSpeed()
-#         elif(self.__action == Action.MOVE_X_NEG):
-#             speed = -self.__propSystem.getSpeed()
-#         elif(self.__action == Action.MOVE_X_STOP):
-#             speed = 0
-#
-#         self.__propSystem.moveX(speed)
-#
-#     def isRepeatable(self):
-#         return False
-
-# class MoveYCommand(Command):
-#     __action = None
-#     __propSystem = None
-#
-#     def __init__(self, action:Action, propSystem:PropulsionSubsystem):
-#         self.__action = action
-#         self.__propSystem = propSystem
-#
-#     def execute(self):
-#         speed = self.__propSystem.getSpeed()
-#
-#         if(self.__action == Action.MOVE_Y_POS):
-#             speed = self.__propSystem.getSpeed()
-#         elif(self.__action == Action.MOVE_Y_NEG):
-#             speed = -self.__propSystem.getSpeed()
-#         elif(self.__action == Action.MOVE_Y_STOP):
-#             speed = 0
-#
-#         self.__propSystem.moveY(speed)
-#
-#     def isRepeatable(self):
-#         return False
 
 class MoveZCommand(Command):
     __action = None
