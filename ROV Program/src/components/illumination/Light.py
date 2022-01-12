21b25f560dcd67cbd736ec3076e17d55a06f9fe4
Tyler <tylernewton08@gmail.com>
2022-01-12 14:43:35 -0500

    Working on lights updates

--- ROV Program/src/components/illumination/Light.py
+++ ROV Program/src/components/illumination/Light.py
@@ -1,45 +1,47 @@
 from abc import ABC, abstractmethod
 
-#Represents a generic light
+#Represents a generic light source
 class Light(ABC):
-    __isOn:bool = False      #Whether or not the light is currently turned on
+    __isActive:bool = False  #Whether or not the light is currently turned on
     __brightness:float = 0.1 #The brightness of the light when turned on
 
     #Turns the light on to its current brightness
     def turnOn(self) -> None:
-        self.__isOn = True
-        self.setState(True)
+        self.__isActive = True
+        self._updateBrightness()
 
     #Turns the light off
     def turnOff(self) -> None:
-        self.__isOn = False
-        self.setState(False)
-
-    #Updates the state(on/off) of the light
-    @abstractmethod
-    def setState(self, isOn:bool) -> None:
-        pass
+        self.__isActive = False
+        self._updateBrightness()
 
     #Sets the brightness of the light
     def setBrightness(self, brightness:float) -> None:
-        #Keeps the brightness in the range [0,1]
+        self.__brightness = self.__boundBrightness(brightness)
+        self.__isActive = self.__brightness > 0
+
+        #Updates the state of the light based on the new brightness
+        self._updateBrightness()
+
+    #Keeps the brightness in the range [0,1]
+    def __boundBrightness(self, brightness:float) -> float:
         if brightness < 0:
             brightness = 0
         elif brightness > 1:
             brightness = 1
 
-        self.__brightness = brightness
+        return brightness
 
-        #Updates the state of the light based on the new brightness
-        if brightness == 0:
-            self.turnOff()
-        else:
-            self.turnOn()
+    #Performs the actual brightness update, since light's can have different
+    #hardware interfaces
+    @abstractmethod
+    def _updateBrightness(self) -> None:
+        pass
 
     #Gets the brightness of the light when turned on
     def getBrightness(self) -> float:
         return self.__brightness
 
     #Gets the state(on/off) of the light
-    def isOn(self) -> bool:
-        return self.__isOn
+    def isActive(self) -> bool:
+        return self.__isActive
--- ROV Program/src/components/illumination/SubseaLight.py
+++ ROV Program/src/components/illumination/SubseaLight.py
@@ -3,26 +3,33 @@ from signals.devices.SignalDevice import SignalDevice
 from signals.PWMSignal import PWMSignal
 from signals.PinMode import PinMode
 
+#Represents the subsea lights used on the ROV from bluerobotics
+#url: https://bluerobotics.com/store/thrusters/lights/lumen-r2-rp/
 class SubseaLight(Light):
-    __pwmSignal:PWMSignal = None
-    LIGHT_OFF:int = 1100
-    LIGHT_MAX:int = 1900
+    __pwmSignal:PWMSignal = None #The PWM signal used to control the light
+    LIGHT_OFF:int = 1100         #The pulse-width value to turn the light off
+    LIGHT_MAX:int = 1900         #The maximim brightness pulse-width for the light
 
     def __init__(self, device:SignalDevice, pinNum:int):
         self.__pwmSignal = PWMSignal(device, pinNum, PinMode.PIN_OUT)
         self.setBrightness(0)
 
-    def setState(self, isOn:bool) -> None:
+    #Performs the brightness update using the PWM interface
+    def _updateBrightness(self) -> None:
         brightness = self.__brightness
 
         #Checks if the light should be turned on or off
-        if not isOn:
+        if not self.__isActive:
             brightness = 0
 
         #Updates the brightness accordingly
         pulseWidth = self.__convertBrightnessToPulseWidth(brightness)
         self.__pwmSignal.setPulseWidth(pulseWidth)
 
+    #Performs the brightness conversion from the range [0,1] to [LIGHT_OFF,LIGHT_MAX]
+    #LIGHT_OFF is the reference point with the total range being the difference between the max
+    #brightness level(LIGHT_MAX) and the off value(LIGHT_OFF). The provided brightness is the used
+    #as a scalar for that range
     def __convertBrightnessToPulseWidth(self, brightness:float) -> int:
         pulseWidth = round(SubseaLight.LIGHT_OFF + ((SubseaLight.LIGHT_MAX - SubseaLight.LIGHT_OFF)) * brightness)
 
