10633bcb21d2b1dee082d1ccaae58ef5d39d36fc
Tyler <tylernewton08@gmail.com>
2022-01-16 18:23:36 -0500

    Moved ResistorType from signals to components package

--- ROV Program/src/components/controllers/RaspberryPi.py
+++ ROV Program/src/components/controllers/RaspberryPi.py
@@ -3,7 +3,7 @@ import os
 import time
 
 from signals.PinMode import PinMode
-from signals.ResistorType import ResistorType
+from components.ResistorType import ResistorType
 from components.controllers.Controller import Controller
 
 #Represents the Raspberry Pi itself (a wrapper for the pigpio library)
