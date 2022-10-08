e018a8da9a22b5b416b43c471dc10e6358439dfb
Tyler <tylernewton08@gmail.com>
2022-10-07 23:56:57 -0400

    Stop having there be duplicates common
    Github is being case sensitive and treating common and Common as different folders

--- common/Connections/build/lib/ROVConnections/Writer.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from ROVMessaging.Message import *
-
-#This is the base class for all writers regardless of how they perform their write
-class Writer(ABC):
-    @abstractmethod
-    def send(self, message:Message) -> None:
-        pass
--- common/Messaging/build/lib/ROVMessaging/SystemStatus.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-#The different states that the system can be in
-class SystemStatus(Enum):
-    INITIALIZING = 0
-    ARMED = 1
-    RUNNING = 2
-    SHUT_DOWN = 3
