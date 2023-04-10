5ce721024e730bff2ac5ddb37278e2dcbf8bd489
mehessli <mehessli@mtu.edu>
2023-04-10 15:14:02 -0400

    Fixed ImageFrame syntax

--- Client Program/src/gui/ImageFrame.py
+++ Client Program/src/gui/ImageFrame.py
@@ -27,29 +27,29 @@ class ImageFrame(Frame, Subscriber):
         #self.grid_columnconfigure(0, weight=1)
         
         #set up grid so that there are 2 columns, the right is bigger than the left
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=3)
 
         #set up grid so that there are 3 rows on the left and one big row on the right
-        self.rowconfigure(0, weight =1)
-        self.rowconfigure(1, weight =1)
-        self.rowconfigure(2, weight =1)
+        self.grid_rowconfigure(0, weight =1)
+        self.grid_rowconfigure(1, weight =1)
+        self.grid_rowconfigure(2, weight =1)
 
         #Image Label
         self.__imageLabel = Label(self, bg = "#00FF00")
         self.__imageLabel.grid(row = 0, column = 1, sticky = "nwes")
 
-	#External Temp label
-	self.__extTempLabel = Label(self, text ="External Tempature: No Data")
-	self.__extTempLabel.grid(column = 0, row = 0, sticky=Tk.N)
+        #External Temp label
+        self.__extTempLabel = Label(self, text ="External Tempature: No Data")
+        self.__extTempLabel.grid(column = 0, row = 0, sticky=Tk.N)
 
-	#Internal Temp label
-	self.__inTempLabel = Label(self, text ="Internal Tempature: No Data")
-	self.__inTempLabel.grid(column = 0, row = 1, sticky=Tk.N)
+        #Internal Temp label
+        self.__inTempLabel = Label(self, text ="Internal Tempature: No Data")
+        self.__inTempLabel.grid(column = 0, row = 1, sticky=Tk.N)
 
-	#Pressure label
-	self.__pressureLabel = Label(self, text ="Pressure: No Data")
-	self.__pressureLabel.grid(column = 0, row = 2, sticky=Tk.N)
+        #Pressure label
+        self.__pressureLabel = Label(self, text ="Pressure: No Data")
+        self.__pressureLabel.grid(column = 0, row = 2, sticky=Tk.N)
 
     #Displays the frame
     def show(self) -> None:
@@ -63,25 +63,25 @@ class ImageFrame(Frame, Subscriber):
         #Checks if the message is new camera frame to display
         if  message.getType() == MessageType.VISION_DATA:
 
-		#Gets the camera frame from the message
-        	frame = message.getContents()["camera"];
+		    #Gets the camera frame from the message
+            frame = message.getContents()["camera"]
+            
+            self.setImage(frame)
 
-		self.setImage(frame)
-
-	#Checks if the message is new sensor data to display
+	    #Checks if the message is new sensor data to display
         if  message.getType() == MessageType.SENSOR_DATA:
 
-		#Get internal temp value and add it to the label
-		inTempValue = message.getContents()["internalTemp"];
-		self.__inTempLabel = Label(self, text ="Internal Tempature: " + str(inTempValue))
+            #Get internal temp value and add it to the label
+            inTempValue = message.getContents()["internalTemp"]
+            self.__inTempLabel = Label(self, text ="Internal Tempature: " + str(inTempValue))
 
-		#Get external temp value and add it to the label
-		extTempValue = message.getContents()["externalTemp"];
-		self.__extTempLabel = Label(self, text ="External Tempature: " + str(extTempValue))
+            #Get external temp value and add it to the label
+            extTempValue = message.getContents()["externalTemp"]
+            self.__extTempLabel = Label(self, text ="External Tempature: " + str(extTempValue))
 
-		#Get pressure value and add it to the label
-		pressureValue = message.getContents()[""pressure""];
-		self.__pressureLabel = Label(self, text ="Pressure: " + str(pressureValu))
+            #Get pressure value and add it to the label
+            pressureValue = message.getContents()["pressure"]
+            self.__pressureLabel = Label(self, text ="Pressure: " + str(pressureValue))
        
 
     #Updates the image shown by the frame
