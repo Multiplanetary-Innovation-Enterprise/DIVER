0c23887224ece91d7eabc658387befce1d51678b
Robert Sirkle <rdsirkle@mtu.edu>
2023-09-05 13:16:05 -0400

    Rewrote UI file for decrease in size and increase in efficiency

--- Client Program/src/gui/UI.py
+++ Client Program/src/gui/UI.py
@@ -1,112 +1,56 @@
-#The program creates a user interface for the Diver ROV Client 
-#Last modified: 9/27/2022
+from tkinter import *
+from PIL import Image,ImageTk
+import cv2
+from loggers.ActionLogger import externaltemp,battery,cameraframe,action
+FakeHardware = False
 
-import tkinter
-from tkinter import ttk, Tk
-import datetime
+#Notes:
+#Resolution: 640x480
 
-class UI(Tk):
+root = Tk(baseName="test")
 
-    #Main method of the program. Calls other functions to create parts of the UI.
-    def _init_(self, master):
-        self.master = master
-        master.title('ROV UI')
+#TODO: hook up real hardware instead of using the stuff below
+temp = 65
+battery = "100%"
+camfeed = cv2.VideoCapture(0)
+logtext = "Test"
 
-        #filler
-        x = 0
+#puts info in labels
+batterydisplay = Label(root,text=battery)
+tempdisplay = Label(root,text=str(temp) + "F")
+camdisplay = Label()
+log = Label(bg="black",fg="white",text=logtext,height=10,anchor=W)
 
-        #set size
-        self.geometry("1000x500")
+button1 = Button(text="hotpockets")
+button1.grid(row=2,column=2,sticky=N)
 
-        #set up grid so that there are 2 columns, the right is bigger than the left
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
-
-        #set up grid so that there are 3 rows on the left and one big row on the right
-        self.rowconfigure(0, weight =1)
-        self.rowconfigure(1, weight =1)
-        self.rowconfigure(2, weight =1)
-
-    #Gets data from external temperature sensor and displays it
-
-        #create frame and set size
-        extTempFrm = Tk.Frame(self)
-        extTempFrm.columnconfigure(0, weight=1)
-
-        #create header label
-        exTTempLabel = ttk.Label(extTempFrm, text ="External Tempature").pack()
-
-        #displays sensor data, displays an error if no data is found
-        if x == 1 :
-            #filler
-            pass
-        else :
-            extTempFailLabel = ttk.Label(extTempFrm, text ="No External Tempature data", foreground = '#f00').pack()
-
-        extTempFrm.grid(column = 0, row = 0, sticky=Tk.N)
-
-        #create frame and set size
-        inTempFrm = Tk.Frame(self)
-        inTempFrm.columnconfigure(0, weight=1) 
-
-        #create header label
-        inTempLabel = ttk.Label(inTempFrm, text ="External Tempature").pack()
-
-         #displays sensor data, displays an error if no data is found
-        if x == 1 :
-            #filler
-            pass
-        else :
-            inTempFailLabel = ttk.Label(inTempFrm, text ="No External Tempature data", foreground = '#f00').pack()
-
-        #return inTempFrm
-        inTempFrm.grid(column = 0, row = 1,sticky=Tk.N)
-
-    #Retrieves battery life from ?? and displays it
-
-        #create frame and set size
-        batFrm = Tk.Frame(root)
-        batFrm.columnconfigure(0, weight=1)
-
-        #create header label
-        batteryLabel = ttk.Label(batFrm, text ="Battery Life").pack() 
-
-        #displays sensor data, displays an error if no data is found
-        if x == 1 :
-            pass
-            #filler
-        else :
-            batteryFailLabel = ttk.Label(batFrm, text ="No battery data", foreground = '#f00').pack()
-
-        #return batFrm
-        batFrm.grid(column = 0, row = 2, sticky=Tk.N)
+#aligning things to grid
+batterydisplay.grid(row=1,column=1,sticky=N)
+tempdisplay.grid(row=1,column=2,sticky=N)
+camdisplay.grid(row=1,column=3)
+#control panel in bottom row
+log.grid(row=2,column=3,sticky=EW)
 
 
-    #Displays camera feed
-
-        #create frame and sset size
-        camFrm = Tk.Frame(root)
-        camFrm.columnconfigure(0, weight=1) 
-
-        #create header label
-        cameraLabel = ttk.Label(camFrm, text ="Camera Feed").pack()
-
-         #displays sensor data, displays an error if no data is found
-        if x == 1:
-            #filler
-             pass
-        else :
-            camFailLabel = ttk.Label(camFrm, text ="No camera data", foreground = '#f00').pack() 
-
-        #return camFrm
-        camFrm.grid(column = 1, row = 0,rowspan = 3, sticky=Tk.N)
+#starts showing video
+def startVideo():
     
-    def update_clock(self):
-        #Update the label
-        self.after(1000, self.update_clock) # run itself again after 1000 ms
+    #pulls a frame as an image
+    _, frame = camfeed.read()
 
-root = Tk()
-rov_UI = UI(root)
+    #Converts image to the proper colors for display
+    displayableImage = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+
+    #converts image to format readable by tkinter
+    Arrayimage = Image.fromarray(displayableImage)
+    photo_image = ImageTk.PhotoImage(image=Arrayimage)
+
+    #sets image in label to frame
+    camdisplay.photo_image = photo_image
+    camdisplay.configure(image=photo_image)
+
+    #repeats this process every 1ms
+    root.after(1,startVideo)
+
+startVideo()
 root.mainloop()
-UI.update_clock()
->>>>>>> 7015b38e1e8a2bec0faaa5b4c7e478130ef7eb26
