a49ed3029f65f9030936e8126b6bc56746f215d8
Robert Sirkle <rdsirkle@mtu.edu>
2023-11-13 10:39:02 -0500

    Fixes startpy to better integrate with SSH

--- Client Program/src/startpy.py
+++ Client Program/src/startpy.py
@@ -5,9 +5,8 @@ sshClient = paramiko.SSHClient()
 sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshClient.connect(socket.gethostbyname("raspberrypi"),port=22,username="pi",password="Mine21",)
 print("Successfully started python on ROV")
-stdin,stdout,stderr = sshClient.exec_command('cd Programs/ROV/src ; python3 ROVLauncher.py')
-for line in stdout.read().splitlines():
-    print(line.decode())
-for line in stderr.read().splitlines():
-    print(line)
-sshClient.close()
+try:
+    stdin,stdout,stderr = sshClient.exec_command('cd Programs/ROV/src ; python3 ROVLauncher.py')
+except KeyboardInterrupt:
+    sshClient.exec_command('sudo shutdown -h now')
+    sshClient.close()
