75eadeb8876cb8244afe84f666e067ee8d32930f
mehessli <mehessli@mtu.edu>
2025-03-14 14:13:12 -0400

    Added MCP9808 as internal temperature sensor
    Replaced DS18820 temperature sensor with MCP9808 temp sensor

---
+++ ROV Program/src/components/sensors/MCP9808TempSensor.py
@@ -0,0 +1,47 @@
+from adafruit_blinka.microcontroller.generic_linux.i2c import I2C
+import board
+import time
+import adafruit_mcp9808
+
+from components.sensors.TempUnit import TempUnit
+from components.sensors.TempSensor import TempSensor
+
+#Represents the MCP9808 Temp Sensor sensor from Adafruit
+#url:https://cdn-learn.adafruit.com/downloads/pdf/adafruit-mcp9808-precision-i2c-temperature-sensor-guide.pdf
+class MCP9808TempSensor(TempSensor):
+    __sensor = None #The Temp Sensor sensor
+
+    def __init__(self):
+        #Attempts to connect to the Temp Sensor
+        try:
+            i2c = board.I2C()
+            self.__sensor = adafruit_mcp9808.MCP980(i2c)
+            self._isConnected = True
+        except ValueError:
+            self._isConnected = False
+            print("Failed to Detect IMU sensor")
+
+
+    #Gets the temperature reading from the sensor
+    def getTemperature(self) -> float:
+        #Skips the temperature reading if the sensor was not found
+        if not self._isConnected:
+            return None
+
+        #Performs the temperature reading, probably unnecessary
+        #tempC = self.__sensor.temperature
+
+        super().getTemperature()
+
+    #Returns the current temperature value in celsius
+    def _getTemperatureC(self) -> float:
+        return self.__sensor.temperature()
+
+    #Returns the current temperature value in fahrenheit
+    def _getTemperatureF(self) -> float:
+        return (self.__sensor.temperature() * 9/5) + 32
+
+    #Returns the current temperature value in fahrenheit
+    def _getTemperatureK(self) -> float:
+        return self.__sensor.temperature() + 273.15
+
