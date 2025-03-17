25e652fd91450c4c2e17c70171e98ecc1f7cc206
Robert Sirkle <rdsirkle@mtu.edu>
2025-03-17 11:34:16 -0400

    Added SCP functionality to startup script

--- Client Program/src/startpy.py
+++ Client Program/src/startpy.py
@@ -1,20 +1,25 @@
 import paramiko
 import socket
+from scp import SCPClient
 from ClientApp import *
 
+#create paramiko SSHClient and connect to Pi
 print("SSHing into ROV...")
 sshClient = paramiko.SSHClient()
 sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshClient.connect(socket.gethostbyname("raspberrypi"),port=22,username="pi",password="Mine21",)
-stdin,stdout,stderr = sshClient.exec_command('cd Programs/ROV/src && nohup "python3 ROVLauncher.py" & exit')
-for line in stdout.read().splitlines():
-    print(line.decode())
-for line in stderr.read().splitlines():
-    print(line)
-print("Successfully started python on ROV")
 
+#use SCP to transfer DIVER folder to Pi
+scpClient = SCPClient(sshClient.get_transport())
+scpClient.put('../../../../DIVER', recursive=True, remote_path='/home/pi/DIVER')
 
+#execute ROV Launcher (without hanging or quitting when the SSH logs out) and then quit 
+sshClient.exec_command('nohup "python3 /home/pi/DIVER/ROV Program/src/ROVLauncher.py" & exit')
+print("Successfully started ROV Launcher on ROV")
 
+sshClient.close()
+
+print("Starting Client Launcher...")
 #Python equivalent of main function
 def main():
     #Starts the ROV client program
