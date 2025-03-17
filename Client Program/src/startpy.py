1e9dfb170b97fcfcbcd4c1ed7535aa6cba4d29a2
Robert Sirkle <rdsirkle@mtu.edu>
2025-03-17 11:14:25 -0400

    Started Conda idea and updated startup script

--- Client Program/src/startpy.py
+++ Client Program/src/startpy.py
@@ -1,13 +1,26 @@
 import paramiko
 import socket
+from ClientApp import *
+
 print("SSHing into ROV...")
 sshClient = paramiko.SSHClient()
 sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshClient.connect(socket.gethostbyname("raspberrypi"),port=22,username="pi",password="Mine21",)
-stdin,stdout,stderr = sshClient.exec_command('cd Programs/ROV/src ; python3 ROVLauncher.py ')
+stdin,stdout,stderr = sshClient.exec_command('cd Programs/ROV/src && nohup "python3 ROVLauncher.py" & exit')
 for line in stdout.read().splitlines():
     print(line.decode())
 for line in stderr.read().splitlines():
     print(line)
-self.sshClient.close()
 print("Successfully started python on ROV")
+
+
+
+#Python equivalent of main function
+def main():
+    #Starts the ROV client program
+    rovClient = ClientApp()
+    rovClient.start()
+
+#Checks that the code is being executed and not imported
+if __name__ == "__main__":
+    main()
